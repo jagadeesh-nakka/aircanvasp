e58b904dcdbe775af9513a40ac48f51de0e53fe5
jagadeesh-nakka <21eg112c31@anurag.edu.in>
2024-07-29 16:46:10 +0530

    update

---
+++ web/web/urls.py
@@ -0,0 +1,35 @@
+'''from django.contrib import admin
+from django.urls import path
+from webapp import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
+    path('signin/', views.signin, name='signin'),
+    path('signout/', views.signout, name='signout'),
+    path('signup/', views.signup, name='signup'),
+    path('upload/', views.upload, name='upload'),
+    path('open_camera/', views.open_camera, name='open_camera'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)'''
+
+from django.urls import path
+from webapp import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.signin, name='signin'),
+    path('signup/', views.signup, name='signup'),
+    path('home/', views.home, name='home'),
+    path('signout/', views.signout, name='signout'),
+    path('upload/', views.upload, name='upload'),
+    path('open_camera/', views.open_camera, name='open_camera'),
+    path('profile/', views.profile, name='profile'),
+    
+
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
